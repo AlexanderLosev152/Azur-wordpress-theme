1050f4563924dc85e72505b6cc3b19f2c647b958
Alexander L <losev.pavlovo152@gmail.com>
2025-07-14 23:43:55 +0300

    first commit

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+add_action( 'wp_enqueue_scripts', function (){
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/style.css');
+	wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/script.js');
+});
+?>
